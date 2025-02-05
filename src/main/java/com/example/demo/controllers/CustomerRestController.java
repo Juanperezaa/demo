27dc0f9abcc5e398c8ecc20478ce3a754a3408e8
Juanperezaa <sebas8041@hotmail.com>
2025-02-05 02:42:46 -0500

    Put, Delete and Patch mapping initial implementation

--- src/main/java/com/example/demo/controllers/CustomerRestController.java
+++ src/main/java/com/example/demo/controllers/CustomerRestController.java
@@ -4,9 +4,12 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,5 +44,51 @@ public class CustomerRestController {
     public customer postCustomer(@RequestBody customer customer){
         customers.add(customer);
         return customer;
+    }
+
+    @PutMapping("/customers")
+    public customer putCustomer(@RequestBody customer customer){
+        for(customer c: customers){
+            if(c.getID() == customer.getID()){
+                c.setName(customer.getName());
+                c.setUsername(customer.getUsername());
+                c.setPassword(customer.getPassword());
+
+                return c;
+            }
+        }
+        return null;
+    }
+
+    @DeleteMapping("/customers/{id}")
+    public customer deleteCustomer(@PathVariable int id){
+        for(customer c: customers){
+            if(c.getID()==id){
+                customers.remove(c);
+
+                return c;
+            }
+        }
+        return null;
+    }
+
+    @PatchMapping("/customers")
+    public customer patchCustomer(@RequestBody customer customer){
+        for(customer c: customers){
+            if(c.getID()==customer.getID()){
+                if(customer.getName()!=null){
+                    c.setName(customer.getName());
+                }
+                if(customer.getUsername()!=null){
+                    c.setUsername(customer.getUsername());
+                }
+                if(customer.getPassword()!=null){
+                    c.setPassword(customer.getPassword());
+                }
+                
+                return c;
+            }
+        }
+        return null;
     }
 }
