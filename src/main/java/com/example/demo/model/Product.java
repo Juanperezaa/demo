90dc40cc354691ff4d6395e34586ea9f54262985
Juanperezaa <sebas8041@hotmail.com>
2025-04-10 02:40:16 -0500

    Included JPA and connected the CRUD to an AWS RDS MYSQL

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -1,12 +1,20 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
 public class Product {
 
+    @Id
     private Integer id;
     private String name;
     private Double price;
     private Integer stock;
     
+
     public Product(){
 
     }
--- src/main/java/com/example/demo/service/ProductServiceImpl.java
+++ src/main/java/com/example/demo/service/ProductServiceImpl.java
@@ -3,87 +3,87 @@ package com.example.demo.service;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Product;
+import com.example.demo.repository.ProductRepository;
 
 @Service
 @ConditionalOnProperty(name = "service.products", havingValue = "listService")
 public class ProductServiceImpl implements ProductService {
 
-    List<Product> productsList = new ArrayList<>(Arrays.asList(
+    @Autowired
+    ProductRepository productRepository;
+    
+   /* List<Product> productsList = new ArrayList<>(Arrays.asList(
         new Product(1, "Laptop", 799.99, 10),
         new Product(2, "Smartphone", 399.99, 15),
         new Product(3, "Tablet", 299.99, 13),
         new Product(4, "Watch", 199.99, 20)
-    ));
+    ));*/ 
 
     @Override
     public List<Product> getProducts(){
-        return productsList;
+        return productRepository.findAll();
     }
 
     @Override
     public Product getProductById(int id) {
-        for(Product p: productsList){
-            if(p.getId()==id){
-                return p;
-            }
-        }
-        return null;
+        return productRepository.findById(id).orElse(new Product());
     }
 
     @Override
     public boolean updateProduct(Product product) {
-        for(Product p: productsList){
-            if(p.getId()==product.getId()){
-                p.setName(product.getName());
-                p.setPrice(product.getPrice());
-                p.setStock(product.getStock());
-                return true;
-            }
+        if (!productRepository.existsById(product.getId())) {
+            return false;
         }
-        return false;
+        productRepository.save(product);
+        return true;
     }
 
     @Override
     public Product createProduct(Product product) {
-        productsList.add(product);
-        return product;
+        return productRepository.save(product);
     }
 
     @Override
     public Product deleteProduct(int id) {
-        for(Product p:productsList){
-            if(p.getId()==id){
-                productsList.remove(p);
-
-                return p;
-            }
-        }
-        return null;
+        return productRepository.findById(id)
+        .map(product -> {
+            productRepository.delete(product);
+            return product;
+        })
+        .orElse(null);
     }
 
     @Override
     public boolean patchProduct(Product product) {
-        for(Product p:productsList){
-            if(p.getId()==product.getId()){
-                if(product.getName()!=null){
-                    p.setName(product.getName());
-                }
-                if(product.getPrice()!=null){
-                    p.setPrice(product.getPrice());
-                }
-                if(product.getStock()!=null){
-                    p.setStock(product.getStock());
-                }
-                return true;
-            }
+        Optional<Product> optionalProduct = productRepository.findById(product.getId());
+
+        if (optionalProduct.isEmpty()) {
+            return false;
         }
-        return false;
+
+        Product existingProduct = optionalProduct.get();
+
+        if (product.getName() != null) {
+            existingProduct.setName(product.getName());
+        }
+        if (product.getPrice() != null) {
+            existingProduct.setPrice(product.getPrice());
+        }
+        if (product.getStock() != null) {
+            existingProduct.setStock(product.getStock());
+        }
+
+        productRepository.save(existingProduct); // Se guarda el producto actualizado
+
+        return true;
     }
     
 }
